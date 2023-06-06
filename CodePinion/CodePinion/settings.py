974bb5377f1ca31c6aaa409c03bb8cefd48bb51c
AlexNdungu <alexmeta517@gmail.com>
2023-06-06 11:23:42 +0300

    Configure Settings py

--- CodePinion/CodePinion/settings.py
+++ CodePinion/CodePinion/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-wx8tk0+7gfn%y+cr$2#2114kpz--_3ogmj2uo1pw_3(!3ks)@q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["codepinion.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Craeted Apps
     'Code1'
 ]
 
