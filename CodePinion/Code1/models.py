b0ac5b641af62e485e7a1b1a6193e20fad8d7b75
AlexNdungu <alexmeta517@gmail.com>
2023-06-16 12:34:41 +0300

    Languages Models write

--- CodePinion/Code1/models.py
+++ CodePinion/Code1/models.py
@@ -17,8 +17,11 @@ class Profile(models.Model):
     full_name = models.CharField(max_length=20, verbose_name='Full Name')
 
     bio = models.TextField(verbose_name='Bio')
+    location = models.TextField(verbose_name='Location')
+    email = models.URLField(max_length = 200, verbose_name='Email')
+    website = models.URLField(max_length = 200, verbose_name='Website')
 
-    profile_pic = models.ImageField(upload_to = 'profiles', default='user.png')
+    profile_pic = models.ImageField(upload_to = 'profiles')
 
     update = models.DateTimeField(auto_now=True)
     created = models.DateField(auto_now_add=True)
@@ -35,3 +38,24 @@ class Profile(models.Model):
     def info_url(self):
         if self.info_picture and hasattr(self.info_picture, 'url'):
             return self.info_picture.url   
+        
+
+#Programming languages models
+class Language(models.Model):
+
+    lang_id = models.AutoField(primary_key=True)
+
+    lang_name = models.CharField(max_length=20, verbose_name='Tag')
+    lang_icon = models.ImageField(upload_to='Tags', verbose_name='Language Icon')
+    lang_desc = models.TextField(verbose_name='Language Description')
+
+    update = models.DateTimeField(auto_now=True)
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.lang_name
+
+    @property
+    def language_url(self):
+        if self.lang_icon and hasattr(self.lang_icon, 'url'):
+            return self.lang_icon.url
