2c1697d1b23fb829eead55a8b0e10ed87b489396
AlexNdungu <alexmeta517@gmail.com>
2023-06-20 11:40:30 +0300

    Connect Jquery

---
+++ CodePinion/Static/Js/Main/path.js
@@ -0,0 +1,9 @@
+//Collecting The Directory to be connected to Code Pinion
+
+let collect_path_btn = document.getElementById('select-path-btn-connect');
+
+collect_path_btn.addEventListener('click', ()=> {
+
+    console.log('click')
+
+})
