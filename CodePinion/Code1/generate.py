d937db9e41b4c41087a02c105b0ad1ffc1e0cfb7
AlexNdungu <alexmeta517@gmail.com>
2023-08-04 19:19:41 +0300

    Username Generator

--- CodePinion/Code1/generate.py
+++ CodePinion/Code1/generate.py
@@ -76,7 +76,7 @@ class UserGen():
         return self.StripMail() + self.GenNumWithMax()
     
 
-    def GenMoreName(self, names_number):
+    def GenAndCompare(self, names_number, custom_list = []):
 
         '''
         This method generates the number of names passed (names_number)
@@ -99,7 +99,7 @@ class UserGen():
 
                     name_generated = self.Combine_Mail_Num()
 
-                    if name_generated not in generated_names:
+                    if name_generated not in generated_names and name_generated not in custom_list:
 
                         generated_names.append(name_generated)
 
