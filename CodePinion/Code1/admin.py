8d480120e479bcf98da976a33fb1eeb709cce4ba
AlexNdungu <alexmeta517@gmail.com>
2023-08-21 13:20:39 +0300

    return dir folders

--- CodePinion/Code1/admin.py
+++ CodePinion/Code1/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
+class SSH_DevicesAdmin(admin.ModelAdmin):
+    exclude = ('host_password',) # exclude the password field
+
+
 admin.site.register(Profile)
 admin.site.register(SSH_Supported)
-admin.site.register(SSH_Devices)
+admin.site.register(SSH_Devices,SSH_DevicesAdmin)
