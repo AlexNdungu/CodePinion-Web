de9da3d71a46938d79b62450325c9fd78e369759
AlexNdungu <alexmeta517@gmail.com>
2023-09-06 23:05:25 +0300

    Code Editor

--- CodePinion/Static/Js/Code Editor/Init_Editor.js
+++ CodePinion/Static/Js/Code Editor/Init_Editor.js
@@ -1,4 +1,5 @@
 // Create a class editor
+let focusIndexNew = null;
 class CodePinionEditor {
 
     // Constructor
@@ -15,15 +16,16 @@ class CodePinionEditor {
         // Create first line
         this.new_line(index,line_number);
 
-        // Call the at_focus_line function
-        this.at_focus_line();
+        // Call at focus line
+        //this.at_focus_line();
 
     }
 
     // Create a new line
     new_line(index,line_number) {
 
-        let line_number_elements = '';
+
+        let line_number_elements = null;
 
         // This is the single line
         let single_line = `
@@ -47,8 +49,6 @@ class CodePinionEditor {
         // Check if the editor is empty
         const hasLines = this.container.hasChildNodes();
 
-        console.log(hasLines);
-
         // If the editor has lines
         if(!hasLines) {
             // Create the first line
@@ -56,8 +56,6 @@ class CodePinionEditor {
 
             // Get the line number
             line_number_elements = document.getElementsByClassName(" line_number");
-
-            console.log(line_number_elements)
             
         }
         else {
@@ -66,40 +64,46 @@ class CodePinionEditor {
 
             // Get the line number
             line_number_elements = document.getElementsByClassName(" line_number");
-
-            // Count the number of lines
-
         }
 
         // Call the monitor line number
         this.monitor_line_number(line_number_elements,index,line_number,);
+
+        // Call at focus line
+        this.at_focus_line();
+        
 
     }
 
     // Monitore the current line being edited
     at_focus_line() {
 
-        let focusIndex = 0;
-
         // Get all the lines
         let all_lines = document.querySelectorAll(".editor_code_line");
 
         // Create a focus event on all_lines
-
         for (let i = 0; i < all_lines.length; i++) {
             all_lines[i].addEventListener('focus', function(event) {
                 // Get the index of the clicked div
-                focusIndex = i;
+                focusIndexNew = i;
+
+                console.log("Focus index: " + focusIndexNew);
+
+                // Add to that line class called is_focused
+                //all_lines[focusIndexNew].classList.add("is_focused");
               
             });
         }
 
-        return focusIndex;
+        return focusIndexNew;
 
     }
 
     // Monitor line number
     monitor_line_number(number_element,line_index,line_number) {
+
+        //console.log("Element: " + number_element);
+        //console.log("Line index: " + line_index);
 
         number_element[line_index].innerHTML = line_number;
 
@@ -110,7 +114,7 @@ class CodePinionEditor {
 
 
 // Onload function
-window.onload = function () {
+//window.onload = function () {
 
     // get the Editor container
     const the_editor = document.getElementById("the_editor");
@@ -128,14 +132,10 @@ window.onload = function () {
     // Create a new line when enter is pressed
     document.onkeydown = function (e) {
         if (e.keyCode == 13) {
-            // index++;
-            // line_number++;
-            // editor.new_line(index,line_number);
-            //console.log(editor.at_focus_line());
-            index = editor.at_focus_line();
+            index = focusIndexNew + 1;
             line_number = index + 1;
             editor.new_line(index,line_number);
         }
     }
 
-}
+//}
