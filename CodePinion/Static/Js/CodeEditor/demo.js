652f50b6e74d23c32dac5bcabf0c5d8f4c4e5dab
AlexNdungu <alexmeta517@gmail.com>
2023-10-21 15:42:15 +0300

    Code Editor Bug Report

--- CodePinion/Static/Js/CodeEditor/demo.js
+++ CodePinion/Static/Js/CodeEditor/demo.js
@@ -1,6 +1,32 @@
 //Get all the elements required for the demo
 let report_bug_btn = document.getElementById("report_bug_btn");
 let csrf = document.getElementsByName('csrfmiddlewaretoken');
+//Here we will give the rich text its fuctionality
+const richButtons = document.querySelectorAll('.btnOption');
+
+// Rich text editor
+richButtons.forEach(richBtn => {
+    richBtn.addEventListener('click', () => {
+
+        let myEvent = richBtn.dataset['command'];
+        
+        //document.execCommand(myEvent, false, null);
+
+        if(myEvent === 'createLink'){
+            let url = prompt('Insert Link Here');
+            document.execCommand(myEvent, false, url);
+        }
+        else if(myEvent === 'formatBlock'){
+            let formattingValue = richBtn.dataset['block'];
+            document.execCommand(myEvent, false, formattingValue);
+        }
+        else{
+            document.execCommand(myEvent, false, null);
+        }
+
+
+    });
+});
 
 //Add event listeners
 report_bug_btn.addEventListener("click", function () {
