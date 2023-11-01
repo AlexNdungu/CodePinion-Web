4a22b9d108e1fa6d94156a593613a283290c7682
AlexNdungu <alexmeta517@gmail.com>
2023-11-01 14:28:19 +0300

    Code Editor Bug Report

--- CodePinion/Static/Js/CodeEditor/demo.js
+++ CodePinion/Static/Js/CodeEditor/demo.js
@@ -4,6 +4,9 @@ let csrf = document.getElementsByName('csrfmiddlewaretoken');
 //Here we will give the rich text its fuctionality
 const richButtons = document.querySelectorAll('.btnOption');
 
+let bodyText = document.getElementById('bodyText');
+let bodyTextContent = document.getElementById('richEdit');
+let bodyTextReview = document.getElementById('bodyTextReview');
 let EditRechTextBtn = document.getElementById('edit_rich_text');
 let ReviewRechTextBtn = document.getElementById('review_rich_text');
 
@@ -32,6 +35,16 @@ richButtons.forEach(richBtn => {
     });
 });
 
+// Add click event to EditRechTextBtn
+EditRechTextBtn.addEventListener("click", function () {
+    edit_rich_text();
+});
+
+// Add click event to ReviewRechTextBtn
+ReviewRechTextBtn.addEventListener("click", function () {
+    review_rich_text();
+});
+
 //Add event listeners
 report_bug_btn.addEventListener("click", function () {
     html2canvas(document.getElementById('the_editor')).then(function (canvas) {
@@ -45,7 +58,30 @@ report_bug_btn.addEventListener("click", function () {
 
 // Review RichText
 function review_rich_text(){
+    // Get all the inner html of the bodyText
+    let bodyTextHtml = bodyTextContent.innerHTML;
+    // Set the inner html of the bodyTextReview
+    bodyTextReview.innerHTML = bodyTextHtml;
+    // Hide the rich text editor
+    bodyText.style.display = "none";
+    // Show the review rich text
+    bodyTextReview.style.display = "block";
+    // Remove from EditRechTextBtn class wrr_btn_color
+    EditRechTextBtn.classList.remove("wrr_btn_color");
+    // Add to ReviewRechTextBtn class wrr_btn_color
+    ReviewRechTextBtn.classList.add("wrr_btn_color");
+}
 
+// Edit RichText
+function edit_rich_text(){
+    // Hide the review rich text
+    bodyTextReview.style.display = "none";
+    // Show the rich text editor
+    bodyText.style.display = "flex";
+    // Remove from ReviewRechTextBtn class wrr_btn_color
+    ReviewRechTextBtn.classList.remove("wrr_btn_color");
+    // Add to EditRechTextBtn class wrr_btn_color
+    EditRechTextBtn.classList.add("wrr_btn_color");
 }
 
 //Function to report a bug
