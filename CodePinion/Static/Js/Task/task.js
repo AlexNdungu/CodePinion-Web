7c550af2851250d98ac7014e90acd2a35ba4773d
AlexNdungu <alexmeta517@gmail.com>
2024-09-25 07:22:12 +0300

    Show Date Picker

--- CodePinion/Static/Js/Task/task.js
+++ CodePinion/Static/Js/Task/task.js
@@ -1,13 +1,25 @@
 let date_reminder_set_addons = document.getElementsByClassName('date_reminder_set_addon');
 let date_reminder_set_addon_inputs = document.getElementsByClassName('date_reminder_set_addon_input');
 let selected_addon_date_views = document.getElementsByClassName('selected_addon_date_view');
+let selected_addon_date_view_spans = document.getElementsByClassName('selected_addon_date_view_span');
 
 function open_date_picker(date_input_index){
-    date_reminder_set_addon_inputs[date_input_index].showPicker()
+    date_reminder_set_addon_inputs[date_input_index].showPicker();
+}
+
+function show_selected_date_in_popup(popup_index,date_value){
+    selected_addon_date_views[popup_index].style.display = 'flex';
+    selected_addon_date_view_spans[popup_index].innerHTML = date_value;
 }
 
 for(let i = 0; i < date_reminder_set_addons.length;i++){
     date_reminder_set_addons[i].addEventListener('click',()=>{
-        open_date_picker(i)
+        open_date_picker(i);
     });
+}
+
+for(let i = 0; i < date_reminder_set_addon_inputs.length;i++){
+    date_reminder_set_addon_inputs[i].addEventListener('change',()=> {
+        show_selected_date_in_popup(i,date_reminder_set_addon_inputs[i].value)
+    })
 }
