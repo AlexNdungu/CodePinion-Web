f54b5a6a4727602cd0beaf16f85a0fd930790552
AlexNdungu <alexmeta517@gmail.com>
2024-09-25 07:04:58 +0300

    Show Date Picker

--- CodePinion/Static/Js/Task/task.js
+++ CodePinion/Static/Js/Task/task.js
@@ -2,15 +2,12 @@ let date_reminder_set_addons = document.getElementsByClassName('date_reminder_se
 let date_reminder_set_addon_inputs = document.getElementsByClassName('date_reminder_set_addon_input');
 let selected_addon_date_views = document.getElementsByClassName('selected_addon_date_view');
 
-function open_date_selectors(date_input){
-    date_input.click()
+function open_date_picker(date_input_index){
+    date_reminder_set_addon_inputs[date_input_index].showPicker()
 }
 
 for(let i = 0; i < date_reminder_set_addons.length;i++){
     date_reminder_set_addons[i].addEventListener('click',()=>{
-        console.log('heloo');
-        console.log(date_reminder_set_addon_inputs[i])
-        date_reminder_set_addon_inputs[i].showPicker()
-        //open_date_selectors(date_reminder_set_addon_inputs[i])
+        open_date_picker(i)
     });
 }
